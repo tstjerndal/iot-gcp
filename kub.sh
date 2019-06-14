c23d3474acf174176e585459edacd69f17cad87c
Tommy Stjerndal <t.stjerndal@abalon.se>
2019-06-14 15:27:23 +0200

    depployfixes

--- kub.sh
+++ kub.sh
@@ -1,6 +1,9 @@
 gcloud container clusters create iot-cluster --num-nodes 1 --machine-type n1-standard-1 --zone europe-west1-b
 gcloud container clusters get-credentials iot-cluster --region europe-west1-b
+
 kubectl run iot-java --image=gcr.io/iot-starvalley-server/iot-java:v1 --port 8080
+eller kubectl apply -f deployment2.yaml
+
 kubectl expose deployment iot-java --type=LoadBalancer
 kubectl create secret generic cloudsql-instance-credentials --from-file credentials.json=../iot-starvalley-server-aff5a7c7c4c0.json
 kubectl create secret generic cloud-db-credentials --from-literal=username=iotUser --from-literal=password=mjaa3579
@@ -11,5 +14,12 @@ chmod +x cloud_sql_proxy
 ./cloud_sql_proxy -instances=iot-starvalley-server:europe-west1:iot-db=tcp:3306
 brew install mysql-client
 /usr/local/opt/mysql-client/bin/mysql --port=3306 --host=127.0.0.1 --password=mjaa3579 --user=iotUser --database=iot-db
+
+
+kubectl get all
+kubectl delete deployment.apps/iot-java
+
+
+kubectl logs deployment.apps/iot-java -c iot-java
 
 
